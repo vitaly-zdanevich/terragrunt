0f679717d54468347f27dea324b0d278d159fdeb
jake lepere <jake.lepere@everlaw.com>
2023-02-10 09:17:34 -0800

    Support running `apply -destroy` in reverse order.

--- configstack/stack_test.go
+++ configstack/stack_test.go
@@ -113,6 +113,31 @@ func TestGetModuleRunGraphDestroyOrder(t *testing.T) {
 		runGraph,
 	)
 
+	terragruntOptions, err := options.NewTerragruntOptionsForTest("")
+	require.NoError(t, err)
+
+	terragruntOptions.TerraformCliArgs = ["-destroy"]
+
+	stack := createTestStack()
+	runGraph, err := stack.getModuleRunGraph(terragruntOptions)
+	require.NoError(t, err)
+
+	assert.Equal(
+		t,
+		[][]*TerraformModule{
+			{
+				stack.Modules[5],
+			},
+			{
+				stack.Modules[3],
+				stack.Modules[4],
+			},
+			{
+				stack.Modules[1],
+			},
+		},
+		runGraph,
+	)
 }
 
 func createTestStack() *Stack {
