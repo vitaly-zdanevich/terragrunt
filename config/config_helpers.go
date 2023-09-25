d3dcba332ee54800328d9c81632c50648159ec18
Simon Engmann <simon.engmann@sovity.de>
2023-09-25 12:42:25 +0200

    Fix relative paths with sops_decrypt_file()
    sops_decrypt_file() computed the file path relative to the working
    directory instead of relative to the Terragrunt configuration. This
    generally worked with individual `terragrunt apply` operations but
    failed for `terragrunt run-all apply`, as it would look for the file in
    the parent directory.
    
    Fixes #1319.

--- config/config_helpers.go
+++ config/config_helpers.go
@@ -743,7 +743,7 @@ func sopsDecryptFile(ctx *ParsingContext, params []string) (string, error) {
 	if err != nil {
 		return "", errors.WithStackTrace(err)
 	}
-	canonicalSourceFile, err := util.CanonicalPath(sourceFile, ctx.TerragruntOptions.WorkingDir)
+	canonicalSourceFile, err := util.CanonicalPath(sourceFile, filepath.Dir(ctx.TerragruntOptions.TerragruntConfigPath))
 	if err != nil {
 		return "", errors.WithStackTrace(err)
 	}
@@ -752,7 +752,7 @@ func sopsDecryptFile(ctx *ParsingContext, params []string) (string, error) {
 		return val, nil
 	}
 
-	rawData, err := decrypt.File(sourceFile, format)
+	rawData, err := decrypt.File(canonicalSourceFile, format)
 	if err != nil {
 		return "", errors.WithStackTrace(err)
 	}
